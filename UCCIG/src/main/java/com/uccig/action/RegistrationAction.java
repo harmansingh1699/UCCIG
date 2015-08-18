a9999f3281f03874ade0782713cd5d5d2d1ff964
ravjot <ravjot28@gmail.com>
2015-08-17 23:32:20 -0400

    Initial Commit
    1. Added struts dependency
    2. Added PostgreSQL and connection pool dependency
    3. Configured struts.xml
    4. Added login and registration action with default success return
    5. Added heroku specific Procfile
    6. Configured build of pom.xml

---
+++ UCCIG/src/main/java/com/uccig/action/RegistrationAction.java
@@ -0,0 +1,47 @@
+package com.uccig.action;
+
+public class RegistrationAction {
+
+	private String userName;
+	private String password;
+	private String repassword;
+	private String emailAddress;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRepassword() {
+		return repassword;
+	}
+
+	public void setRepassword(String repassword) {
+		this.repassword = repassword;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String execute() {
+		return "success";
+
+	}
+
+}
